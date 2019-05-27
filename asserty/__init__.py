704f6ef51ddf640a583c2956986ad0fe4252e874
Jonathan <jonathal@axis.com>
2019-05-27 12:30:17 +0200

    Add better failure messages

--- asserty/__init__.py
+++ asserty/__init__.py
@@ -1,7 +1,7 @@
 from .asserts import Assert
 from .expected_error import  expected_error
 
-version = "0.0.1"
+version = "0.0.3"
 name = "asserty"
 
 
--- asserty/asserts.py
+++ asserty/asserts.py
@@ -4,64 +4,64 @@ from unittest import TestCase
 testcase_instance = TestCase("__init__")
 
 
-def assert_true(obj):
-    testcase_instance.assertTrue(obj)
+def assert_true(obj, msg=""):
+    testcase_instance.assertTrue(obj, msg)
 
 
-def assert_false(obj):
-    testcase_instance.assertFalse(obj)
+def assert_false(obj, msg=""):
+    testcase_instance.assertFalse(obj, msg)
 
 
-def assert_none(obj):
-    testcase_instance.assertIsNone(obj)
+def assert_none(obj, msg=""):
+    testcase_instance.assertIsNone(obj, msg)
 
 
-def assert_not_none(obj):
-    testcase_instance.assertIsNotNone(obj)
+def assert_not_none(obj, msg=""):
+    testcase_instance.assertIsNotNone(obj, msg)
 
 
-def assert_equal(a, b):
-    testcase_instance.assertEqual(a, b)
+def assert_equal(a, b, msg=""):
+    testcase_instance.assertEqual(a, b, msg)
 
 
-def assert_not_equal(a, b):
-    testcase_instance.assertNotEqual(a, b)
+def assert_not_equal(a, b, msg=""):
+    testcase_instance.assertNotEqual(a, b, msg)
 
 
-def assert_greater_than(a, b):
-    testcase_instance.assertGreater(a, b)
+def assert_greater_than(a, b, msg=""):
+    testcase_instance.assertGreater(a, b, msg)
 
 
-def assert_greater_equal_than(a, b):
-    testcase_instance.assertGreaterEqual(a, b)
+def assert_greater_equal_than(a, b, msg=""):
+    testcase_instance.assertGreaterEqual(a, b, msg)
 
 
-def assert_less_than(a, b):
-    testcase_instance.assertLess(a, b)
+def assert_less_than(a, b, msg=""):
+    testcase_instance.assertLess(a, b, msg)
 
 
-def assert_less_equal_than(a, b):
-    testcase_instance.assertLessEqual(a, b)
+def assert_less_equal_than(a, b, msg=""):
+    testcase_instance.assertLessEqual(a, b, msg)
 
 
 def assert_raises(err, func, *args, **kwargs):
     testcase_instance.assertRaises(err, func, *args, **kwargs)
 
 
-def assert_type(obj, t):
-    testcase_instance.assertIsInstance(obj, t)
+def assert_type(obj, t, msg=""):
+    testcase_instance.assertIsInstance(obj, t, msg)
 
 
-def assert_not_type(obj, t):
-    testcase_instance.assertNotIsInstance(obj, t)
+def assert_not_type(obj, t, msg=""):
+    testcase_instance.assertNotIsInstance(obj, t, msg)
 
 
-def assert_contains(coll, obj):
-    testcase_instance.assertIn(obj, coll)
+def assert_contains(coll, obj, msg=""):
+    testcase_instance.assertIn(obj, coll, msg)
 
 
-def assert_not_contains(coll, obj):
-    testcase_instance.assertNotIn(obj, coll)
+def assert_not_contains(coll, obj, msg=""):
+    testcase_instance.assertNotIn(obj, coll, msg)
 
 
 class Assert:
@@ -78,37 +78,43 @@ class Assert:
 
     def is_none(self):
         """Assert that this has a value of None."""
-        assert_none(self.value)
+        msg = "Expected {} to be None".format(self.value)
+        assert_none(self.value, msg)
 
     def is_not_none(self):
         """Assert that this DO NOT has a value of None."""
-        assert_not_none(self.value)
+        msg = "Expected {} to be None".format(self.value)
+        assert_not_none(self.value, msg)
 
     def equals(self, other):
         """Assert that if this object is equal to the other object."""
-        assert_equal(self.value, other)
+        msg = "Expected {} to equal".format(self.value, other)
+        assert_equal(self.value, other, msg)
         return self
 
     def not_equals(self, other):
         """Check that if this object DO NOT equal to the other object."""
-        assert_not_equal(self.value, other)
+        msg = "Expected {} not to equal".format(self.value, other)
+        assert_not_equal(self.value, other, msg)
         return self
 
     def has_type(self, expected):
         """Assert that this has the expected type."""
         if not isinstance(expected, type):
             expected = type(expected)
-        assert_type(self.value, expected)
+        msg = "Expected {} to have type {} but was {}".format(self.value, type(self.value), expected)
+        assert_type(self.value, expected, msg)
         return self
 
     has_same_type_as = has_type
     is_instance = has_type
 
-    def not_has_type(self, other):
+    def not_has_type(self, expected):
         """Assert that this NOT have the expected type."""
-        if not isinstance(other, type):
-            other = type(other)
-        assert_not_type(self.value, other)
+        if not isinstance(expected, type):
+            expected = type(expected)
+        msg = "Expected {} not to have type {} but was".format(self.value, expected)
+        assert_not_type(self.value, expected, msg)
         return self
 
     not_has_same_type_as = not_has_type
@@ -116,46 +122,54 @@ class Assert:
 
     def is_in(self, coll):
         """Assert that this is in the given collection."""
-        assert_contains(coll, self.value)
+        msg = "Expected {} to be in {} but was not".format(self.value, coll)
+        assert_contains(coll, self.value, msg)
         return self
 
     def is_not_in(self, coll):
         """Assert that this is NOT in the given collection."""
-        assert_not_contains(coll, self.value)
+        msg = "Expected {} not to be in {} but was".format(self.value, coll)
+        assert_not_contains(coll, self.value, msg)
         return self
 
     def has_length(self, expected):
         """Assert that this is of the expected length"""
         if hasattr(expected, "__len__"):
             expected = len(expected)
-        assert_equal(len(self.value), expected)
+        msg = "Expected {} to have length {} but was {}".format(self.value, expected, len(self.value))
+        assert_equal(len(self.value), expected, msg)
         return self
 
     def not_has_length(self, expected):
         """Assert that this is NOT of the expected length"""
         if hasattr(expected, "__len__"):
             expected = len(expected)
-        assert_not_equal(len(self.value), expected)
+        msg = "Expected {} to have length {} but was".format(self.value, expected)
+        assert_not_equal(len(self.value), expected, msg)
         return self
 
     def is_greater_than(self, other):
         """Assert that this is greater than other."""
-        assert_greater_than(self.value, other)
+        msg = "Expected {} to be greater than {}".format(self.value, other)
+        assert_greater_than(self.value, other, msg)
         return self
 
     def is_greater_or_equal_to(self, other):
         """Assert that this is greater than other."""
-        assert_greater_equal_than(self.value, other)
+        msg = "Expected {} to be greater or equal to {}".format(self.value, other)
+        assert_greater_equal_than(self.value, other, msg)
         return self
 
     def is_less_than(self, other):
         """Assert that this is greater than other."""
-        assert_less_than(self.value, other)
+        msg = "Expected {} to be less than {}".format(self.value, other)
+        assert_less_than(self.value, other, msg)
         return self
 
     def is_less_or_equal_to(self, other):
         """Assert that this is greater than other."""
-        assert_less_equal_than(self.value, other)
+        msg = "Expected {} to be less or equal to {}".format(self.value, other)
+        assert_less_equal_than(self.value, other, msg)
         return self
     
     # Bool Assertions
@@ -173,31 +187,36 @@ class Assert:
 
     def contains(self, obj):
         """Assert that this contain the given object."""
-        assert_contains(self.value, obj)
+        msg = "Expected {} to contain {}".format(self.value, obj)
+        assert_contains(self.value, obj, msg)
         return self
 
     def not_contains(self, obj):
         """Assert that this DO NOT contain the given object."""
-        assert_not_contains(self.value, obj)
+        msg = "Expected {} not to contain {}".format(self.value, obj)
+        assert_not_contains(self.value, obj, msg)
         return self
 
     def contains_key(self, key):
         """Assert that this contains the given key."""
         if not hasattr(self.value, "__contains__"):
             raise TypeError("value has no keys")
-        assert_contains(self.value, key)
+        msg = "Expected {} to contain key {}".format(self.value, key)
+        assert_contains(self.value, key, msg)
         return self
 
     def not_contains_key(self, key):
         """Assert that this DO NOT contains the given key."""
         if not hasattr(self.value, "__contains__"):
             raise TypeError("value has no keys")
-        assert_not_contains(self.value, key)
+        msg = "Expected {} not to contain key {}".format(self.value, key)
+        assert_not_contains(self.value, key, msg)
         return self
 
     def has_same_elements(self, other):
         """Assert that this and the other collection has the same elements."""
-        assert_equal(sorted(self.value), other)
+        msg = "Expected {} and {} to contain the same elements".format(self.value, other)
+        assert_equal(sorted(self.value), other, msg)
         return self
     
     # Callable assertions
@@ -205,6 +224,8 @@ class Assert:
     @property
     def when_called(self):
         """Assert if called without arguments."""
+        if not callable(self.value):
+            raise TypeError("object asserted is not callable")
         self.args = []
         self.kwargs = {}
         return self
@@ -213,64 +234,75 @@ class Assert:
 
     def when_called_with(self, *args, **kwargs):
         """Assert if called with the given arguments."""
+        if not callable(self.value):
+            raise TypeError("object asserted is not callable")
         self.args = args
         self.kwargs = kwargs
         return self
 
     if_called_with = when_called_with
 
-    def raises(self, err):
+    def raises(self, err: type):
         """Assert if called that the given error is raised."""
-
         assert_raises(err, self.value, *self.args, **self.kwargs)
 
     def returns(self, expected):
         """Assert if called that the given result is returned."""
         actual = self.value(*self.args, **self.kwargs)
-        assert_equal(actual, expected)
+        msg = "Expected {} but {} was returned".format(expected, actual)
+        assert_equal(actual, expected, msg)
     
     # HTTP Assertions
+    
+    def has_status_successful(self):
+        msg = "Expected successful HTTP status code but was {}".format(self.value.status_code)
+        assert_less_than(self.value.status_code, 400, msg)
+        return self
 
     def has_status_ok(self):
-        assert_equal(self.value.status_code, 200)
-        return self
-
-    def has_status_successful(self):
-        assert_less_than(self.value.status_code, 400)
-        return self
+        return self._has_status(200)
 
     def has_status_created(self):
-        assert_equal(self.value.status_code, 201)
-        return self
+        return self._has_status(201)
 
     def has_status_no_content(self):
-        assert_equal(self.value.status_code, 204)
-        return self
+        return self._has_status(204)
 
     def has_status_bad_request(self):
-        assert_equal(self.value.status_code, 400)
-        return self
+        return self._has_status(400)
 
     def has_status_unauthorized(self):
-        assert_equal(self.value.status_code, 401)
-        return self
+        return self._has_status(401)
 
     def has_status_forbidden(self):
-        assert_equal(self.value.status_code, 403)
-        return self
+        return self._has_status(403)
 
     def has_status_not_found(self):
-        assert_equal(self.value.status_code, 404)
-        return self
+        return self._has_status(404)
 
     def has_status_method_not_allowed(self):
-        assert_equal(self.value.status_code, 405)
+        return self._has_status(405)
+
+    def has_status_precondition_failed(self):
+        return self._has_status(412)
+    
+    def _has_status(self, expected: int):
+        msg = "Expected HTTP status {} but was {}".format(expected, self.value.status_code)
+        assert_equal(self.value.status_code, expected, msg)
         return self
 
     def body_equals(self, body: dict):
-        assert_equal(self.value.json(), body)
+        msg = "Expected body {} to equal {}".format(self.value.json(), body)
+        assert_equal(self.value.json(), body, msg)
+        return self
+
+    def body_length(self, length: int):
+        actual = len(self.value.json())
+        msg = "Expected body length to be {} but was {}".format(actual, length)
+        assert_equal(actual, length, msg)
         return self
 
     def body_contains_key(self, key: str):
-        assert_contains(self.value.json(), key)
+        msg = "Expected body {} to contain key {}".format(self.value.json(), key)
+        assert_contains(self.value.json(), key, msg)
         return self
